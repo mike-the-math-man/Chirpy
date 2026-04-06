7307af762e97db2c445e905e5c7c223647bf8882
mike_the_sloth <mjooste13@gmail.com>
2026-04-06 13:20:35 +0200

    lost track of time added json writing and error writing 2:30

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -28,6 +29,52 @@ func (cfg *apiConfig) handler_reset(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("Hits: %d", 0)))
 }
 
+type validate_chirp struct {
+	Body string `json:"body"`
+}
+
+type error_response struct {
+	Error string `json:"error"`
+}
+
+type valid_response struct {
+	Valid bool `json:"valid"`
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, error_response{Error: msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func validate_chirp_handler(w http.ResponseWriter, r *http.Request) {
+
+	decoder := json.NewDecoder(r.Body)
+	params := validate_chirp{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		respondWithError(w, 500, "Error decoding parameters")
+		return
+	}
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+	respondWithJSON(w, 200, valid_response{Valid: true})
+}
+
 func main() {
 	const filepathRoot = "."
 	const port = ":8080"
@@ -42,6 +89,7 @@ func main() {
 		})
 	serverMux.HandleFunc("GET /admin/metrics", apiCfg.handler_metric)
 	serverMux.HandleFunc("POST /admin/reset", apiCfg.handler_reset)
+	serverMux.HandleFunc("POST /api/validate_chirp", validate_chirp_handler)
 	server := http.Server{
 		Addr:    port,
 		Handler: serverMux,
