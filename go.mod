8fe1d8cf7f3011920ab1a90a30f8edda8d1213e6
mike_the_sloth <mjooste13@gmail.com>
2026-04-12 21:00:13 +0200

    database and user creation

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/mike-the-math-man/Chirpy.git
 
 go 1.25.1
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.12.3 
+	github.com/pressly/goose/v3 v3.27.0
+)
--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/mike-the-math-man/Chirpy.git/internal/database"
 )
 
 type apiConfig struct {
-	fileserverHits atomic.Int32
+	fileserverHits  atomic.Int32
+	databaseQueries *database.Queries
+	env_platform    string
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -27,8 +37,13 @@ func (cfg *apiConfig) handler_metric(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) handler_reset(w http.ResponseWriter, r *http.Request) {
+	if cfg.env_platform != "dev" {
+		w.WriteHeader(403)
+		return
+	}
 	cfg.fileserverHits.Store(0)
 	w.Write([]byte(fmt.Sprintf("Hits: %d", 0)))
+	cfg.databaseQueries.DeleteUsers(r.Context())
 }
 
 type validate_chirp struct {
@@ -42,6 +57,17 @@ type error_response struct {
 type valid_response struct {
 	Valid       bool   `json:"valid"`
 	CleanedBody string `json:"cleaned_body"`
+}
+
+type user_email struct {
+	Email string `json:"email"`
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func cleanInput(s string, words []string) string {
@@ -81,6 +107,30 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Write(dat)
 }
 
+func (cfg *apiConfig) users_handler(w http.ResponseWriter, r *http.Request) {
+
+	decoder := json.NewDecoder(r.Body)
+	params := user_email{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		respondWithError(w, 500, "Error decoding parameters")
+		return
+	}
+	user, err := cfg.databaseQueries.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		log.Printf("Error creating user: %s", err)
+		respondWithError(w, 500, "Error creating user")
+		return
+	}
+	user_struct := User{}
+	user_struct.CreatedAt = user.CreatedAt
+	user_struct.UpdatedAt = user.UpdatedAt
+	user_struct.Email = user.Email
+	user_struct.ID = user.ID
+	respondWithJSON(w, 201, user_struct)
+}
+
 func validate_chirp_handler(w http.ResponseWriter, r *http.Request) {
 
 	decoder := json.NewDecoder(r.Body)
@@ -100,9 +150,19 @@ func validate_chirp_handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	dbQueries := database.New(db)
+	if err != nil {
+		return
+	}
 	const filepathRoot = "."
 	const port = ":8080"
 	var apiCfg apiConfig
+	platform := os.Getenv("PLATFORM")
+	apiCfg.env_platform = platform
+	apiCfg.databaseQueries = dbQueries
 	serverMux := http.NewServeMux()
 	serverMux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
 	serverMux.HandleFunc("GET /api/healthz",
@@ -114,6 +174,7 @@ func main() {
 	serverMux.HandleFunc("GET /admin/metrics", apiCfg.handler_metric)
 	serverMux.HandleFunc("POST /admin/reset", apiCfg.handler_reset)
 	serverMux.HandleFunc("POST /api/validate_chirp", validate_chirp_handler)
+	serverMux.HandleFunc("POST /api/users", apiCfg.users_handler)
 	server := http.Server{
 		Addr:    port,
 		Handler: serverMux,
